782835d80fdebfc0b49dda35368895eb30457b20
SolPhua <solomonphua7@gmail.com>
2026-01-17 22:46:42 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 console.log("script loaded");
 
 document.getElementById("continueBtn").addEventListener("click", () => {
-  alert("Continue button works 🐼💖");
+  document.querySelector(".card").innerHTML = `
+    <h1>Will you be my Valentine? 🐼💘</h1>
+    <button id="yesBtn">YES 💕</button>
+    <button id="noBtn">NO 😐</button>
+  `;
+
+  document.getElementById("yesBtn").addEventListener("click", () => {
+    document.querySelector(".card").innerHTML = `
+      <h1>YAYYYY 🐼🎉</h1>
+      <p>14 Feb, 7pm.<br>Your panda is yours 💖</p>
+    `;
+  });
+
+  const noBtn = document.getElementById("noBtn");
+  noBtn.addEventListener("mouseover", () => {
+    noBtn.style.position = "absolute";
+    noBtn.style.left = Math.random() * 70 + "vw";
+    noBtn.style.top = Math.random() * 70 + "vh";
+  });
 });
